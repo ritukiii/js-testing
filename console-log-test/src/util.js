48e79ec9e0ac36ab95245bd435d49efdf0119c46
ritukiii <rikkey0000qwe@gmail.com>
2016-11-14 01:08:08 +0100

    write test using console

---
+++ console-log-test/src/util.js
@@ -0,0 +1,7 @@
+function jadge(mark) {
+    if (mark <= 30) {
+        return '赤点です';
+    } else {
+        return '合格です';
+    }
+}
---
+++ console-log-test/test/util-spec.js
@@ -0,0 +1,9 @@
+// ブラウザ上で結果を確認出来るようにconsoleにログを吐き出す
+console.log('test');
+console.log('if the mark is under 30, return 赤点');
+
+if (jadge(30) === "赤点です") {
+    console.log("成功");
+} else {
+    console.log("失敗");
+}
