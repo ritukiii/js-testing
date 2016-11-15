bbe379e08997139d8fe6cb8eb2ccb917c87ccdf9
ritukiii <rikkey0000qwe@gmail.com>
2016-11-15 16:14:16 +0100

    fix comment

--- using-mocha/test/util-spec.js
+++ using-mocha/test/util-spec.js
@@ -1,9 +1,9 @@
 // ブラウザ上でmochaを使ってテスト結果を出力する
-
 describe('jadgeのテスト', function() {
   it('30点以下なら赤点を返す', function() {
     if (jadge(30) === '赤点') {
     } else {
+      // 期待しない結果の時は例外を投げる 
       throw new Error('失敗');
     }
   });
